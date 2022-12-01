b45e0cb44856b0449afacc5aa9fa539c4b826350
ProtossDragoon <dlwkdgn1@naver.com>
2022-12-01 21:07:53 +0900

    feat: 파일 로깅 기능 추가

--- log.py
+++ log.py
@@ -1,16 +1,16 @@
-import sys
 import logging
+import sys
 
 
 class CustomFormatter(logging.Formatter):
 
-    grey = "\x1b[38;20m"
-    yellow = "\x1b[33;20m"
-    green = "\x1b[32;21m"
-    red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
-    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    grey = '\x1b[38;20m'
+    yellow = '\x1b[33;20m'
+    green = '\x1b[32;21m'
+    red = '\x1b[31;20m'
+    bold_red = '\x1b[31;1m'
+    reset = '\x1b[0m'
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
     FORMATS = {
         logging.DEBUG: grey + format + reset,
@@ -26,14 +26,33 @@ class CustomFormatter(logging.Formatter):
         return formatter.format(record)
 
 
+class FileOutputFormatter(logging.Formatter):
+
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+    FORMATS = {
+        logging.DEBUG: format,
+        logging.INFO: format,
+        logging.WARNING: format,
+        logging.ERROR: format,
+        logging.CRITICAL: format
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 def set_default_logger(level=logging.INFO, logfile_path='e2e.log'):
     logger = logging.getLogger()
     logger.setLevel(level=level)
-    
+
     consoleHandler = logging.StreamHandler(stream=sys.stdout)
     consoleHandler.setFormatter(CustomFormatter())
     logger.addHandler(consoleHandler)
-    
-    fileHandler = logging.FileHandler(filename=logfile_path, encoding='utf-8-sig')
-    fileHandler.setFormatter(CustomFormatter())
+
+    fileHandler = logging.FileHandler(
+        filename=logfile_path, encoding='utf-8-sig')
+    fileHandler.setFormatter(FileOutputFormatter())
     logger.addHandler(fileHandler)
